a13d813ea3298f55297621f7ba13a7a202601f00
Dmitry <zik-element@mail.ru>
2021-10-02 15:11:13 +0300

    dz4

---
+++ src/components/Pagination.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="filterNum > 1">
+      &#60;
+      <button v-for="n in filterNum" :key="n" @click="filterItems(n)">
+        {{ n }}
+      </button>
+      &#62;
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    itemsLength: {
+      type: Number
+    }
+  },
+  data () {
+    return {
+      idItem: 0,
+      filterNum: 4
+    }
+  },
+  methods: {
+    filterItems (n) {
+      const namberPage = n
+      this.$emit('selectPage', namberPage)
+    }
+  }
+}
+</script>
--- src/components/PaymantDisplay.vue
+++ src/components/PaymantDisplay.vue
@@ -7,20 +7,11 @@
       <div class="item-cont">Котегория</div>
       <div class="item-cont">Значение</div>
     </div>
-    <div v-for="(item, idx) in items" :key="idx">
-      <div class="contents" v-if="idx >= min && idx < max">
+    <div v-for="(item, idx) in items.slice(this.numberPage * 3 - 3, this.numberPage * 3)" :key="idx" class="contents">
         <div class="item-cont">{{ item.id }}</div>
         <div class="item-cont">{{ item.date }}</div>
         <div class="item-cont">{{ item.type }}</div>
         <div class="item-cont">{{ item.amount }}</div>
-      </div>
-    </div>
-    <div v-if="filterNum > 1">
-      &#60;
-      <button v-for="n in filterNum" :key="n" @click="filterItems(n)">
-        {{ n }}
-      </button>
-      &#62;
     </div>
   </div>
 </template>
@@ -36,34 +27,31 @@ export default {
     showItems: {
       type: Boolean,
       default: false
+    },
+    numberPage: {
+      type: Number,
+      default: 1
     }
   },
   data () {
     return {
       idItem: 0,
       filterNum: 0,
-      quantityItem: 5,
-      min: 0,
-      max: 5
+      filterItem: ''
     }
   },
-  methods: {
-    filterItems (n) {
-      this.max = this.quantityItem * n
-      this.min = this.max - this.quantityItem
-    }
-  },
+  methods: {},
   created () {
-    this.idItem = this.items[this.items.length - 1].id
+    // this.idItem = this.items[this.items.length - 1].id
     this.$emit('idCreated', this.idItem)
-    this.filterNum = Math.floor(this.items.length / 5) + 1
+    this.filterItem = this.items.slice(this.numberPage * 3 - 3, this.numberPage * 3)
   },
   mounted () {
   },
   updated () {
-    this.idItem = this.items[this.items.length - 1].id
+    // this.idItem = this.items[this.items.length - 1].id
     this.$emit('idUpdate', this.idItem)
-    this.filterNum = Math.floor(this.items.length / 5) + 1
+    this.filterItem = this.items.slice(this.numberPage * 3 - 3, this.numberPage * 3)
   }
 }
 </script>
